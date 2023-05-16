c22fff1165fc250a5c055bd23948787eb4cd148a
HieuLeXuan <hieulexuan629@gmail.com>
2023-05-16 21:08:56 +0700

    comple preactice asic knowlege

--- RNCourse practice unit 2/App.js
+++ RNCourse practice unit 2/App.js
@@ -1,31 +1,37 @@
-import { useState } from 'react'
-import { StyleSheet, View, Button, FlatList } from 'react-native'
+import { useState } from "react";
+import { StyleSheet, View, Button, FlatList } from "react-native";
 
-import GoalInput from './components/GoalInput'
-import GoalItem from './components/GoalItem'
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
 
 export default App = () => {
-  const [visibleIsModal, setVisibleIsModal] = useState(false)
-  const [courseGoals, setCourseGoals] = useState([])
+  const [visibleIsModal, setVisibleIsModal] = useState(false);
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const openModalHandler = () => {
-    setVisibleIsModal(true)
-  }
+    setVisibleIsModal(true);
+  };
 
   const addGoalHandler = (enteredGoal) => {
     if (!enteredGoal) {
-      return
+      return;
     }
     setCourseGoals((currentGoal) => [
       ...currentGoal,
       { text: enteredGoal, id: Math.random().toString() },
-    ])
-    closeModalHandler()
-  }
+    ]);
+    closeModalHandler();
+  };
 
   const closeModalHandler = () => {
-    setVisibleIsModal(false)
-  }
+    setVisibleIsModal(false);
+  };
+
+  const deleteGoalHandler = (idGoal) => {
+    setCourseGoals((currentGoal) =>
+      courseGoals.filter((goal) => goal.id !== idGoal)
+    );
+  };
 
   return (
     <View style={styles.appContainer}>
@@ -41,12 +47,18 @@ export default App = () => {
       />
       <FlatList
         data={courseGoals}
-        renderItem={(itemData) => <GoalItem data={itemData.item} />}
+        renderItem={(itemData) => (
+          <GoalItem
+            data={itemData.item}
+            onDeleteGoal={deleteGoalHandler}
+            id={itemData.item.id}
+          />
+        )}
         keyExtractor={(item) => item.id}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -56,5 +68,6 @@ const styles = StyleSheet.create({
   },
   addGoalBtn: {
     borderRadius: 6,
+    backgroundColor: "#342156",
   },
-})
+});
--- RNCourse practice unit 2/components/GoalItem.js
+++ RNCourse practice unit 2/components/GoalItem.js
@@ -1,22 +1,30 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { useState } from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export default GoalItem = (props) => {
-  console.log('goal item:', props.data)
   return (
-    <View style={styles.item}>
-      <Text style={styles.itemText}>{props.data.text}</Text>
+    <View style={styles.itemGoal}>
+      <Pressable
+        onPress={props.onDeleteGoal.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.itemText}>{props.data.text}</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  item: {
+  itemGoal: {
     borderRadius: 6,
-    padding: 8,
-    marginTop: 8,
-    backgroundColor: '#342156',
+    margin: 5,
+    backgroundColor: "#342156",
+  },
+  pressedItem: {
+    opacity: 0.2,
   },
   itemText: {
-    color: 'white',
+    padding: 8,
+    color: "white",
   },
-})
+});
